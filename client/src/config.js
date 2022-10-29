0028fc0fe19a1baf20f249b201ab1a6b67361de1
Rakshit Shrivastava <92736675+Rakshit-Shrivastava@users.noreply.github.com>
2022-10-29 20:06:32 +0530

    added vercel file

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-// const host = "http://localhost:3000";
-const host = "https://mern-crud-app-dusky.vercel.app";
+const host = "http://localhost:3000";
+// const host = "https://mern-crud-app-dusky.vercel.app";
 
 export default host;
